0d7004eeaf9d7d1ffd3beeca66d66d2855c7aa4f
carloscoss <crobertohenriquez@gmail.com>
2026-05-24 22:15:40 -0500

    Adding chips class functionalities

--- src/main/java/com/pluralsight/model/Chips.java
+++ src/main/java/com/pluralsight/model/Chips.java
@@ -1,4 +1,22 @@
 package com.pluralsight.model;
 
-public class Chips {
+public class Chips implements IOrderable {
+    private String name;
+    private double price;
+
+
+
+
+    @Override
+    public double getPrice(){
+        return price;
+    }
+
+    @Override
+    public String getStringDetails(){
+        return name + " Chips";
+    }
+
+
+
 }
